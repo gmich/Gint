c6aa271ab046390c789d77f8de5aaa0ce48f949c
Giorgos Michaelides <g.mich90@gmail.com>
2021-09-26 02:30:11 +0200

    Add pipe helper extensions

---
+++ src/Gint.Pipes/PipeHelperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace Gint.Pipes
+{
+    public static class PipeHelperExtensions
+    {
+        public static string AsUTF8String(this byte[] bytes)
+        {
+            var utf8 = Encoding.UTF8 as UTF8Encoding;
+            return utf8.GetString(bytes);
+        }
+
+        public static byte[] AsUTF8ByteArray(this string utf8String)
+        {
+            var utf8 = Encoding.UTF8 as UTF8Encoding;
+            return utf8.GetBytes(utf8String);
+        }
+    }
+}
